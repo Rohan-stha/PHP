13d5edda16a2a084a9dd3a7acbd6753c65722543
Rohan Shrestha <rohan.org.com@gmail.com>
2026-09-01 01:07:39 +0545

    add constants

--- Constants.php
+++ Constants.php
@@ -1,5 +1,5 @@
 <?php
-// name or identifier that cant e changed during the execution of the script except for magic constants
+// name or identifier that cant  changed during the execution of the script except for magic constants
 // define() function is used to create a constant
 // define("CONSTANT_NAME", "value");
 //const keyword is used to create a constant
