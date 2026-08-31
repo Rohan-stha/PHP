ba31d3961412dde1f47ad84797ffee6fb14eb0f4
Rohan Shrestha <rohan.org.com@gmail.com>
2026-09-01 01:05:40 +0545

    add php constants

---
+++ Constants.php
@@ -0,0 +1,14 @@
+<?php
+// name or identifier that cant e changed during the execution of the script except for magic constants
+// define() function is used to create a constant
+// define("CONSTANT_NAME", "value");
+//const keyword is used to create a constant
+// const CONSTANT_NAME = "value";
+
+
+define("GREETING", "Hello, World!");
+echo GREETING;
+const PI = 3.14;
+echo "<br>".PI;
+
+?>
