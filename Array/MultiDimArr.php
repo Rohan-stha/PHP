6b8afae3b9b02fd4d47547fcd391ef94651726bc
Rohan Shrestha <rohan.org.com@gmail.com>
2026-09-01 08:23:53 +0545

    add multidimensional array

--- Array/MultiDimArr.php
+++ Array/MultiDimArr.php
@@ -22,4 +22,30 @@ echo "Jane is " . $familes['Jane']['age'] . " years old and lives in " . $famile
 echo "Bob is " . $familes['Bob']['age'] . " years old and lives in " . $familes['Bob']['city'] . ".<br>";//output: Bob is 35 years old and lives in Chicago.
 echo "Harry is " . $familes['Harry']['age'] . " years old and lives in " . $familes['Harry']['city'] . ".<br>";//output: Harry is 40 years old and lives in Kathmandu.
 
+Array(
+    [Newar] => Array
+        (
+            [0] => Hari
+            [1] => Shyam
+        ),
+
+        [Brahman] => Array
+        (
+            [0] => Sita
+            [1] => Gita
+        ),
+        [Chhetri] => Array
+        (
+            [0] => Ram
+            [1] => Laxman
+        )
+
+)
+echo "Hari is a " . $familes['Newar'][0] . ".<br>";//output: Hari is a Newar.
+echo "Shyam is a " . $familes['Newar'][1] . ".<br>";//output: Shyam is a Newar.
+echo "Sita is a " . $familes['Brahman'][0] . ".<br>";//output: Sita is a Brahman.
+echo "Gita is a " . $familes['Brahman'][1] . ".<br>";//output: Gita is a Brahman.
+echo "Ram is a " . $familes['Chhetri'][0] . ".<br>";//output: Ram is a Chhetri.
+echo "Laxman is a " . $familes['Chhetri'][1] . ".<br>";//output: Laxman is a Chhetri.
+
 ?>
