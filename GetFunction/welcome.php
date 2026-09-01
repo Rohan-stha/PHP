9ced2d413717993782190d6167410018e21ac265
Rohan Shrestha <rohan.org.com@gmail.com>
2026-09-02 00:51:11 +0545

    add get function

--- GetFunction/welcome.php
+++ GetFunction/welcome.php
@@ -1,6 +1,6 @@
 
 <?php 
 
-echo "<h1>Welcome, " . $_GET['name'] . "!</h1>";
+echo "<h1>Welcome, " . $_GET['name'] . "!</h1>";//In url data are visible in the address bar. For example, if you visit the URL "welcome.php?name=John&age=30", it will display "Welcome, John!" and "You are 30 years old." on the page.
 echo "<p>You are " . $_GET['age'] . " years old.</p>";
 ?>
