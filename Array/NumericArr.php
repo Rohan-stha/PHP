c7ac4985de7dd10655e769eeee29170308aa2265
Rohan Shrestha <rohan.org.com@gmail.com>
2026-09-01 07:54:29 +0545

    add array

---
+++ Array/NumericArr.php
@@ -0,0 +1,14 @@
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2]."cars.<br>";
+
+
+
+$fruit[0] = "Banana";
+$fruit[1] = "Apple";
+$fruit[2] = "Orange";
+$fruit[3] = "Mango";
+
+echo "I like " . $fruit[0] . ", " . $fruit[1] . " and " . $fruit[2] . " and " . $fruit[3] ." " . "fruits.";
+
+?>
